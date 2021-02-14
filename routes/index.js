8770a503b3a3ad6ff4fbc18233f436a29fbfdfcb
yuanjingya2017 <17664051304@163.com>
2021-02-14 23:52:48 +0800

    feat: init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var user = require('../models/index');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/addUser', function(req, res, next) {
+  user.add(req, res, next);
+});
 module.exports = router;
